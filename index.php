003c3153138b908da8809a638a8e8e2c708fb07d
Leszek Pietrzak <lpietrzak@gog.com>
2016-08-25 12:08:46 +0200

    some cleanups in theme files

--- index.php
+++ index.php
@@ -6,8 +6,6 @@
 
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-
-
         <article <?php post_class() ?> id="post-<?php the_ID(); ?>">
 
             <h1><a href="<?php the_permalink() ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h1>
