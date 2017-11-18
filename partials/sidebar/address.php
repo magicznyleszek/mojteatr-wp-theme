49a8d3d25b1671aa93f5b0638a94b502669074e7
Leszek Pietrzak <smutnyleszek@gmail.com>
2017-11-18 21:35:29 +0100

    final tweaks

--- partials/sidebar/address.php
+++ partials/sidebar/address.php
@@ -3,10 +3,22 @@
 
     Gorczyczewskiego 2/1A<br>
     60-554 Poznań<br>
-    <a i-o-button href="http://g.co/maps/vjw2" title="Idź do mapy">MAPA</a>
+    <a
+        i-o-button
+        href="http://g.co/maps/vjw2"
+        target="_blank"
+        title="Zobacz Teatr na mapie"
+    >
+        MAPA: Teatr
+    </a>
 
-    <a i-o-button href="https://goo.gl/maps/nMeC4mF9Xsn" title="Parking">
-        Parking
+    <a
+        i-o-button
+        href="https://goo.gl/maps/nMeC4mF9Xsn"
+        target="_blank"
+        title="Zobacz parking na mapie"
+    >
+        MAPA: Parking
         <span i-o-button-tooltip>Ponieważ przy ul. Gorczyczewskiego jest mało miejsc parkingowych, nasi widzowie często korzystają z płatnego parkingu przy Centrum Stomatologii (wjazd od ul. Rokietnickiej)</span>
     </a>
 </section>
