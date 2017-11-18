83b1ff6664c87390106f8ce09b51dfbc0b298e4d
Leszek Pietrzak <smutnyleszek@gmail.com>
2017-11-18 16:30:49 +0100

    parking link

--- partials/sidebar/address.php
+++ partials/sidebar/address.php
@@ -3,5 +3,11 @@
 
     Gorczyczewskiego 2/1A<br>
     60-554 Poznań<br>
+    60-554 Poznań<br>
     <a i-o-button href="http://g.co/maps/vjw2" title="Idź do mapy">MAPA</a>
+
+    <a i-o-button href="https://goo.gl/maps/nMeC4mF9Xsn" title="Parking">
+        Parking
+        <span i-o-button-tooltip>Ponieważ przy ul. Gorczyczewskiego jest mało miejsc parkingowych, nasi widzowie często korzystają z płatnego parkingu przy Centrum Stomatologii (wjazd od ul. Rokietnickiej)</span>
+    </a>
 </section>
