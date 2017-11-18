c2d85d85684af8079ee7a1033990cbd929d549c4
Leszek Pietrzak <smutnyleszek@gmail.com>
2017-11-18 21:14:13 +0100

    deduplicate address

--- partials/sidebar/address.php
+++ partials/sidebar/address.php
@@ -3,7 +3,6 @@
 
     Gorczyczewskiego 2/1A<br>
     60-554 Poznań<br>
-    60-554 Poznań<br>
     <a i-o-button href="http://g.co/maps/vjw2" title="Idź do mapy">MAPA</a>
 
     <a i-o-button href="https://goo.gl/maps/nMeC4mF9Xsn" title="Parking">
