52fcd27abee5363d9f3fb5e7c4762484bd3758a2
Leszek Pietrzak <leszek@magicznyleszek.xyz>
2021-02-09 13:19:31 +0000

    remove marek

--- partials/sidebar/contact.php
+++ partials/sidebar/contact.php
@@ -2,16 +2,8 @@
     <h4>Kontakt</h4>
 
     <div i-o-section>
-        Informacje:<br>
         Hanna Kostołowska<br>
         +48 667 341 705<br>
         <a href="mailto:biuro@mojteatr.pl">biuro@mojteatr.pl</a>
-    </div>
-
-    <div i-o-section>
-        Dyrektor artystyczny:<br>
-        Marek Zgaiński<br>
-        <a href="mailto:m.zgainski@mojteatr.pl">m.zgainski@mojteatr.pl</a><br>
-        +48 668 856 444
     </div>
 </section>
