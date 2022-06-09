e142203af4c393b40b43ed28089f801fe41fac09
Leszek <smutnyleszek@gmail.com>
2022-06-09 17:56:46 +0200

    hide sidebar stuff

--- sidebar.php
+++ sidebar.php
@@ -1,8 +1,10 @@
 <sidebar id="sidebar">
     <?php include (TEMPLATEPATH . '/partials/sidebar/tickets.php'); ?>
+    <!--
     <?php include (TEMPLATEPATH . '/partials/sidebar/address.php'); ?>
     <?php include (TEMPLATEPATH . '/partials/sidebar/contact.php'); ?>
     <?php include (TEMPLATEPATH . '/partials/sidebar/searchbox.php'); ?>
     <?php include (TEMPLATEPATH . '/partials/sidebar/friends.php'); ?>
     <?php include (TEMPLATEPATH . '/partials/sidebar/patrons.php'); ?>
+    -->
 </sidebar>
